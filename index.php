99b6af390860ad2f94438fabdc9b80a9bb0fe1c6
mbusson <mbusson@users.noreply.github.com>
2016-04-26 00:56:09 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Maxence Busson | Front-End Developer</title>
+	 <link rel="stylesheet" href="styles.css">
+	 <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+	<div id="loader-wrapper">
+	    <div id="loader"></div>
+	    <div class="loader-section section-left"></div>
+	    <div class="loader-section section-right"></div>
+	</div>
+	<div id="gradient"></div>
+	<div id="bg-ol"></div>
+
+	<div id="logo" class="element">
+		<h1>Maxence Busson</h1>
+	</div>
+	<header  class="element">
+		<h2>Front-End Web Development</h2>
+	</header>
+
+	<div id="network">
+		<a style="display:block" href="carousel/index.html">
+			<div class="netw element">
+				<div id="cp"></div>
+			</div>
+		</a>
+		<div id="content"></div>
+		<a style="display:block" href="carousel/index.html">
+			<div class="netwL element">
+				<div id="git"></div>
+			</div>
+		</a>
+	</div>
+
+	<div id="sites">
+		<a style="display:block" href="carousel/index.html"><div class="site element" id="site1">
+			<img class="thumbnail" src="images/site1.jpg"><br><br>
+			<div class="arrow-wrap"><i class="fa fa-angle-double-up" aria-hidden="true"></i></div><br><br>
+		</div></a>
+		<a style="display:block" href="carousel/index.html"><div class="site element" id="site2">
+			<img class="thumbnail" src="images/site1.jpg"><br><br>
+			<div class="arrow-wrap"><i class="fa fa-angle-double-up" aria-hidden="true"></i></div><br><br>
+		</div></a>
+		<a style="display:block" href="carousel/index.html"><div class="site element" id="site3">
+			<img class="thumbnail" src="images/site1.jpg"><br><br>
+			<div class="arrow-wrap"><i class="fa fa-angle-double-up" aria-hidden="true"></i></div><br><br>
+		</div></a>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="style.js"></script>
+</body>
+</html>
